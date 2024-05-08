47b40b73c8566b3b5550115dc3962fafae66aacf
shining013 <nevertheless113@naver.com>
2024-05-08 21:36:13 +0900

    feat: LackOfShortenUrlKeyException 핸들러

--- src/main/java/kr/co/shortenurlservice/presentation/GlobalExceptionHandler.java
+++ src/main/java/kr/co/shortenurlservice/presentation/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package kr.co.shortenurlservice.presentation;
 
+import kr.co.shortenurlservice.domain.LackOfShortenUrlKeyException;
 import kr.co.shortenurlservice.domain.NotFoundShortenUrlException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,5 +12,9 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NotFoundShortenUrlException.class)
     public ResponseEntity<String> handleNotFoundShortenUrlException(NotFoundShortenUrlException ex) {
         return new ResponseEntity<>("단축 URL을 찾지 못했습니다.", HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(LackOfShortenUrlKeyException.class)
+    public ResponseEntity<String> handleLackOfShortenUrlKeyException(LackOfShortenUrlKeyException ex) {
+        return new ResponseEntity<>("단축 URL 자원이 부족합니다.", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
