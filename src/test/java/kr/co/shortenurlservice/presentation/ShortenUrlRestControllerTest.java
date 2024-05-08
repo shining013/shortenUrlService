2efd688fec802c749d4879dd60481793cb6b4c01
shining013 <nevertheless113@naver.com>
2024-05-08 22:25:56 +0900

    feat: 컨트롤러 단위 테스트

---
+++ src/test/java/kr/co/shortenurlservice/presentation/ShortenUrlRestControllerTest.java
@@ -0,0 +1,32 @@
+package kr.co.shortenurlservice.presentation;
+
+import kr.co.shortenurlservice.application.SimpleShortenUrlService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = ShortenUrlRestController.class)
+class ShortenUrlRestControllerTest {
+    @MockBean
+    private SimpleShortenUrlService simpleShortenUrlService;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("원래의 URL로 리다이렉트 되어야 한다.")
+    void redirectTest() throws Exception {
+        String expectedOriginalUrl = "https://www.hanbit.co.kr/";
+        when(simpleShortenUrlService.getOriginalUrlByShortenUrlKey(any())).thenReturn(expectedOriginalUrl);
+        mockMvc.perform(get("/any-key"))
+                .andExpect(status().isMovedPermanently())
+                .andExpect(header().string("Location", expectedOriginalUrl));
+    }
+}
