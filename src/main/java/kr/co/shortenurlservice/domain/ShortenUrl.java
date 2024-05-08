45b730b2d840010db02a7e8b032ce6ce22521c37
shining013 <nevertheless113@naver.com>
2024-05-08 11:51:14 +0900

    feat: 단축 URL 생성 메서드 구현

--- src/main/java/kr/co/shortenurlservice/domain/ShortenUrl.java
+++ src/main/java/kr/co/shortenurlservice/domain/ShortenUrl.java
@@ -1,7 +1,22 @@
 package kr.co.shortenurlservice.domain;
 
+import java.util.Random;
+
 public class ShortenUrl {
     private String originalUrl;
     private String shortenUrlKey;
     private Long redirectCount;
+
+    public static String generateShortenUrlKey() {
+        String base56Characters = "23456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz";
+        Random random = new Random();
+        StringBuilder shortenUrlKey = new StringBuilder();
+
+        for (int count = 0; count < 8; count++) {
+            int base56CharactersIndex = random.nextInt(0, base56Characters.length());
+            char base56Character = base56Characters.charAt(base56CharactersIndex);
+            shortenUrlKey.append(base56Character);
+        }
+        return shortenUrlKey.toString();
+    }
 }
