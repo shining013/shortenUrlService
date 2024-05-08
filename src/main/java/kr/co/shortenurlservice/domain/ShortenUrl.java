ad96b1b5b863e18e747c9e8d9604b55dadca2d5f
shining013 <nevertheless113@naver.com>
2024-05-08 12:07:18 +0900

    feat: 단축 URL 클래스

--- src/main/java/kr/co/shortenurlservice/domain/ShortenUrl.java
+++ src/main/java/kr/co/shortenurlservice/domain/ShortenUrl.java
@@ -7,6 +7,24 @@ public class ShortenUrl {
     private String shortenUrlKey;
     private Long redirectCount;
 
+    public ShortenUrl(String originalUrl, String shortenUrlKey) {
+        this.originalUrl = originalUrl;
+        this.shortenUrlKey = shortenUrlKey;
+        this.redirectCount = 0L;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public String getShortenUrlKey() {
+        return shortenUrlKey;
+    }
+
+    public Long getRedirectCount() {
+        return redirectCount;
+    }
+
     public static String generateShortenUrlKey() {
         String base56Characters = "23456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz";
         Random random = new Random();
