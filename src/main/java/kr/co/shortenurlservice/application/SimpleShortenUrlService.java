fbcd89b3fd715a696f18cedbfca4e6798a3cdb4b
shining013 <nevertheless113@naver.com>
2024-05-08 11:45:40 +0900

    feat: 단축 URL 서비스 클래스 생성자

--- src/main/java/kr/co/shortenurlservice/application/SimpleShortenUrlService.java
+++ src/main/java/kr/co/shortenurlservice/application/SimpleShortenUrlService.java
@@ -1,11 +1,17 @@
 package kr.co.shortenurlservice.application;
 
+import kr.co.shortenurlservice.domain.ShortenUrlRepository;
 import kr.co.shortenurlservice.presentation.ShortenUrlCreateRequestDto;
 import kr.co.shortenurlservice.presentation.ShortenUrlCreateResponseDto;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SimpleShortenUrlService {
+    private ShortenUrlRepository shortenUrlRepository;
+
+    SimpleShortenUrlService(ShortenUrlRepository shortenUrlRepository) {
+        this.shortenUrlRepository = shortenUrlRepository;
+    }
     public ShortenUrlCreateResponseDto generateShortenUrl(ShortenUrlCreateRequestDto shortenUrlCreateRequestDto) {
         return null;
     }
