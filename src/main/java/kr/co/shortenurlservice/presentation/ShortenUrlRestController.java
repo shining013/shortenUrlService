948625dfe9b8afe871c7b02dcd5a21c97934b66b
shining013 <nevertheless113@naver.com>
2024-05-08 12:58:50 +0900

    feat: redirect 컨트롤러

--- src/main/java/kr/co/shortenurlservice/presentation/ShortenUrlRestController.java
+++ src/main/java/kr/co/shortenurlservice/presentation/ShortenUrlRestController.java
@@ -1,10 +1,16 @@
 package kr.co.shortenurlservice.presentation;
 
-import jakarta.validation.Valid;
 import kr.co.shortenurlservice.application.SimpleShortenUrlService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import jakarta.validation.Valid;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @RestController
 public class ShortenUrlRestController {
@@ -23,8 +29,13 @@ public class ShortenUrlRestController {
     }
 
     @RequestMapping(value = "/{shortenUrlKey}", method =  RequestMethod.GET)
-    public ResponseEntity<?> redirectShortenUrl(@PathVariable String shortenUrlKey) {
-        return ResponseEntity.ok().body(null);
+    public ResponseEntity<?> redirectShortenUrl(@PathVariable String shortenUrlKey) throws URISyntaxException {
+        String originalUrl = simpleShortenUrlService.getOriginalUrlByShortenUrlKey(shortenUrlKey);
+        URI redirectUri = new URI(originalUrl);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(redirectUri);
+
+        return new ResponseEntity<>(httpHeaders, HttpStatus.MOVED_PERMANENTLY);
     }
     @RequestMapping(value = "/shortenUrl/{shortenUrlKey}", method = RequestMethod.GET)
     public ResponseEntity<ShortenUrlInformationDto> getShortenUrlInformation(@PathVariable String shortenUrlKey) {
